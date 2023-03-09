2c51bd563390749c7098fc872f37d2485ab78c52
Yerke <zh.yerkegaly@gmail.com>
2023-03-09 18:30:22 +0800

    Initial Commit

---
+++ Twitter Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter Tutorial
+//
+//  Created by Yerkegali Zhamalbekov on 09.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
